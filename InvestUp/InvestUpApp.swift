f6d8b1aad33cf20f1e66e489d8a848284a57a239
fgoosev <fgoosev@gmail.com>
2023-04-19 20:29:53 +0400

    Initial Commit

---
+++ InvestUp/InvestUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestUpApp.swift
+//  InvestUp
+//
+//  Created by Александр Гусев on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct InvestUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
